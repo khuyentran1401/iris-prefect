ebc3e33b066fc1ddfe870d84cedadf6dab772a5d
Khuyen Tran <khuyentran1476@gmail.com>
2022-12-19 11:53:16 -0600

    add validation

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from typing import List, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class DataLocation(BaseModel):
@@ -10,11 +10,23 @@ class DataLocation(BaseModel):
 
 
 class ProcessConfig(BaseModel):
-    drop_columns: list = ["Id"]
+    drop_columns: List[str] = ["Id"]
     label: str = "Species"
     test_size: float = 0.3
+
+    @validator("test_size")
+    def must_be_non_negative(cls, v):
+        if v < 0:
+            raise ValueError(f"{v} must be non-negative")
+        return v
 
 
 class SVC_Params(BaseModel):
     C: List[float] = [0.1, 1, 10, 100, 1000]
     gamma: List[float] = [1, 0.1, 0.01, 0.001, 0.0001]
+
+    @validator("*", each_item=True)
+    def must_be_non_negative(cls, v):
+        if v < 0:
+            raise ValueError(f"{v} must be non-negative")
+        return v
