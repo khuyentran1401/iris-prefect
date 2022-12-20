dd74c48d6f790ff106878bfd36769ae6f8263464
Khuyen Tran <khuyentran1476@gmail.com>
2022-12-20 09:30:19 -0600

    add process w/o prefect

--- src/process_without_prefect.py
+++ src/process_without_prefect.py
@@ -1,28 +1,21 @@
 import pandas as pd
-from prefect import flow, task
 from sklearn.model_selection import train_test_split
 
-from config import DataLocation, ProcessConfig
 
-
-@task
 def get_raw_data(data_location: str, file_name: str):
     return pd.read_csv(f"{data_location}/{file_name}")
 
 
-@task
 def drop_columns(data: pd.DataFrame, columns: list):
     return data.drop(columns=columns)
 
 
-@task
 def get_X_y(data: pd.DataFrame, label: str):
     X = data.drop(columns=label)
     y = data[label]
     return X, y
 
 
-@task
 def split_train_test(X: pd.DataFrame, y: pd.DataFrame, test_size: int):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, random_state=0
@@ -35,23 +28,25 @@ def split_train_test(X: pd.DataFrame, y: pd.DataFrame, test_size: int):
     }
 
 
-@task
 def save_processed_data(data: dict, save_location: str):
     for name, df in data.items():
         df.to_pickle(f"{save_location}/{name}")
 
 
-@flow(log_prints=True)
 def process(
-    data_location: DataLocation = DataLocation(),
-    config: ProcessConfig = ProcessConfig(),
+    raw_location: str = "data/raw",
+    process_location: str = "data/processed",
+    raw_file: str = "iris.csv",
+    label: str = "Species",
+    test_size: float = 0.3,
+    columns_to_drop=["Id"],
 ):
-    data = get_raw_data(data_location.raw_location, data_location.raw_file)
-    processed = drop_columns(data, config.drop_columns)
-    X, y = get_X_y(processed, config.label)
-    split_data = split_train_test(X, y, config.test_size)
-    save_processed_data(split_data, data_location.process_location)
+    data = get_raw_data(raw_location, raw_file)
+    processed = drop_columns(data, columns=columns_to_drop)
+    X, y = get_X_y(processed, label)
+    split_data = split_train_test(X, y, test_size)
+    save_processed_data(split_data, process_location)
 
 
 if __name__ == "__main__":
-    process(config=ProcessConfig(test_size=-0.1))
+    process(test_size=0.4, columns_to_drop=["col1", "col2"])
